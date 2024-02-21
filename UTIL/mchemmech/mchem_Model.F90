e307ca95e8377fbb9ac847c8244af113c1b41148
Simon Rosanka <srosanka@uci.edu>
2024-02-21 15:57:53 -0800

    MCHEM: Add KPP generated files to repository

---
+++ UTIL/mchemmech/mchem_Model.F90
@@ -0,0 +1,20 @@
+MODULE mchem_Model
+
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!  Completely defines the model mchem
+!    by using all the associated modules
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  USE mchem_Precision
+  USE mchem_Parameters
+  USE mchem_Global
+  USE mchem_Function
+  USE mchem_Integrator
+  USE mchem_Rates
+  USE mchem_Jacobian
+  USE mchem_LinearAlgebra
+  USE mchem_Monitor
+!  USE mchem_Util
+
+END MODULE mchem_Model
+
